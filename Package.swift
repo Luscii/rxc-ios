083e7807e8c775315533d563df57976a0845aa45
hungrxyz <marko@hungry.dev>
2023-08-31 09:42:43 +0200

    Adds binary targets as libraries to Package.swift

--- Package.swift
+++ Package.swift
@@ -12,9 +12,32 @@ let package = Package(
     products: [
         .library(
             name: "RxSwift",
-            targets: ["Actions"]),
+            targets: ["RxSwift"]
+        ),
+        .library(
+            name: "RxCocoa",
+            targets: ["RxCocoa"]
+        ),
+        .library(
+            name: "RxRelay",
+            targets: ["RxRelay"]
+        ),
     ],
     targets: [
-        
+        .binaryTarget(
+            name: "RxSwift",
+            url: "https://github.com/Luscii/rxc-ios/releases/download/6.6.0/RxSwift.xcframework.zip",
+            checksum: "d5b890d0b99c358574303f0370b4f8c12bbf111d41882cc6c3993206d76fff56"
+        ),
+        .binaryTarget(
+            name: "RxCocoa",
+            url: "https://github.com/Luscii/rxc-ios/releases/download/6.6.0/RxCocoa.xcframework.zip",
+            checksum: "81c8de8e419f20a1f194340c5a88c6e84d04edc1ffef25b36c1570fc43de7502"
+        ),
+        .binaryTarget(
+            name: "RxRelay",
+            url: "https://github.com/Luscii/rxc-ios/releases/download/6.6.0/RxRelay.xcframework.zip",
+            checksum: "932e6bb853264cdace0d291e2c69aa680a3d0e764b8071d6e63a133e7e8b661b"
+        )
     ]
 )
